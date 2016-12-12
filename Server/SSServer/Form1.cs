bdfdb5210b0f6a3e849197305f6c6ca8a5955b45
mnopkat <mnopkat@gmail.com>
2016-12-13 00:29:48 +0300

    Server

---
+++ Server/SSServer/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace SSServer
+{
+    public partial class Form1 : Form
+    {
+        private static string _port;
+        private static bool state;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Visible = false;
+            this.IsVisibilityChangeAllowed = false;
+        }
+        
+        bool IsVisibilityChangeAllowed { get; set; }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (this.IsVisibilityChangeAllowed)
+            {
+                base.SetVisibleCore(value);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Dispose();
+            Application.ExitThread();
+            Application.Exit();
+        }
+    }
+}
---
+++ Server/SSServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SSServer
+{
+    static class Program
+    {
+
+        private static NotifyIcon notifyIcon = null;
+        private static string _port;
+        private static bool state;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.Icon;
+            //notifyIcon.MouseDoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            notifyIcon.Visible = true;
+            startt();
+            Application.Run();
+        }
+        public static void startt()
+        {
+            SerialPort por1 = new SerialPort();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                _port = port;
+            }
+            while (true)
+            {
+                por1.PortName = _port;
+                por1.BaudRate = 9600;
+                por1.Open();
+                state = get_socket_state();
+
+                if (state == true)
+                {
+                    por1.Write("on");
+                    notifyIcon.Text = "Умная розетка v1.0.0\nСостояние розетки: включена";
+                }
+                else if (state == false)
+                {
+                    por1.Write("of");
+                    notifyIcon.Text = "Умная розетка v1.0.0\nСостояние розетки: выключена";
+                }
+
+                else notifyIcon.Text = "Умная розетка v1.0.0\nПроизошла ошибка...";
+                por1.Close();
+                Thread.Sleep(500);
+            }
+        }
+
+        public static bool get_socket_state()
+        {
+            WebRequest req = HttpWebRequest.Create("http://relaxcs.by/andrewkharkov/ss/status");
+            Stream s = req.GetResponse().GetResponseStream();
+            StreamReader sr = new StreamReader(s);
+            string state = sr.ReadToEnd();
+            sr.Close();
+            if (state == "true")
+                return true;
+            else
+                return false;
+        }
+        private static void notifyIcon1_DoubleClick(object Sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
